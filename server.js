a87405bc31d1c70ea87eca1d867285fe35fa5e87
Eric Barbosa <ericferrari98@hotmail.com>
2023-01-15 12:50:07 -0300

    update

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-// import { json } from 'body-parser';
-// import jsonServer from 'json-server';
-// const server = jsonServer.create();
-// const router = jsonServer.router('db.json');
-// const middlewares =  jsonServer.defaults();
-// const port = process.env.PORT. || 3200;
-
-// server.use(middlewares);
-// server.use(router);
-
-// server.listen(port);
-
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
